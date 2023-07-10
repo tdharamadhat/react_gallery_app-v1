d6ed4fe42c0a00573c1a5b5e3043a131a00dc7b4
tdharamadhat <tdharamadhat@gmail.com>
2023-07-10 13:43:42 +0700

    hide message

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
   useEffect(() => {
     initializeData();
     return () => {
-      console.log('Load Finish...');
+      //console.log('Load Finish...');  debug
     }
    }, [initializeData]) ;
  
